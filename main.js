37c503997acf338eeab45e658ccb51bd96ac0dc2
pling0041-alt <pling0041@gmail.com>
2025-11-11 14:04:45 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  // 추후 여기에 트렌드 페이지 연결
+  win.loadURL('https://www.youtube.com/feed/trending');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
